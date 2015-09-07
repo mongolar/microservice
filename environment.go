ea914f13e7f2d84bde9cb920ae26926a79f008c6
Jason Smith <jasonrichardsmith@gmail.com>
2015-09-07 11:28:06 -0400

    a littlemore organization, moved environment to its own file

---
+++ environment.go
@@ -0,0 +1,80 @@
+package service
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+const vulcanpath = "/vulcand/backends"
+
+var env Environment
+
+func init() {
+	port := flag.String("port", "", "The microservice port.")
+	host := flag.String("host", "", "The microservice host.")
+	etcdmachines := flag.String("etcd", "", "The etcd machines.")
+	flag.Parse()
+	if *port == "" {
+		log.Fatal(errors.New("Port parameter is required."))
+	}
+	if *host == "" {
+		var err error
+		host, err = getEnvHost()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	if *etcdmachines == "" {
+		var err error
+		etcdmachines, err = getEnvEtcdMachines()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	env = Environment{
+		Port:     *port,
+		Machines: strings.Split(*etcdmachines, "|"),
+		Host:     *host,
+		URL:      fmt.Sprintf("http://%v:%v", *host, *port),
+	}
+}
+
+type Environment struct {
+	Port     string   `json:"-"`
+	Host     string   `json:"-"`
+	Machines []string `json:"-"`
+	URL      string   `json:"URL"`
+}
+
+func (e *Environment) refresh() {
+	for _ = range time.Tick(10 * time.Second) {
+		etcdmachines, err := getEnvEtcdMachines()
+		if err != nil || *etcdmachines != "" {
+			// TODO pass error to service here
+			fmt.Println(err)
+		} else {
+			env.Machines = strings.Split(*etcdmachines, "|")
+		}
+	}
+}
+
+func getEnvHost() (*string, error) {
+	host := os.Getenv("MONGOLAR_SERVICES_HOST")
+	if host == "" {
+		return &host, errors.New("MONGOLAR_SERVICES_HOST is not set, service host environement value is required.")
+	}
+	return &host, nil
+}
+
+func getEnvEtcdMachines() (*string, error) {
+	etcdmachines := os.Getenv("MONGOLAR_ETCD_MACHINES")
+	if etcdmachines == "" {
+		return &etcdmachines, errors.New("MONGOLAR_ETCD_MACHINES is not set, etcd machines environmental value is required.")
+	}
+	return &etcdmachines, nil
+}
--- service.go
+++ service.go
@@ -2,69 +2,14 @@ package service
 
 import (
 	"encoding/json"
-	"errors"
-	"flag"
 	"fmt"
 	"github.com/coreos/go-etcd/etcd"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 	"time"
 )
-
-const vulcanpath = "/vulcand/backends"
-
-var env Environment
-
-func init() {
-	port := flag.String("port", "", "The microservice port.")
-	host := flag.String("host", "", "The microservice host.")
-	etcdmachines := flag.String("etcd", "", "The etcd machines.")
-	flag.Parse()
-	if *port == "" {
-		log.Fatal(errors.New("Port parameter is required."))
-	}
-	if *host == "" {
-		var err error
-		host, err = getEnvHost()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	if *etcdmachines == "" {
-		var err error
-		etcdmachines, err = getEnvEtcdMachines()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	env = Environment{
-		Port:     *port,
-		Machines: strings.Split(*etcdmachines, "|"),
-		Host:     *host,
-		URL:      fmt.Sprintf("http://%v:%v", *host, *port),
-	}
-}
-
-type Environment struct {
-	Port     string   `json:"-"`
-	Host     string   `json:"-"`
-	Machines []string `json:"-"`
-	URL      string   `json:"URL"`
-}
-
-func (e *Environment) refresh() {
-	etcdmachines, err := getEnvEtcdMachines()
-	if err != nil {
-		// TODO pass error to service here
-		fmt.Println(err)
-	} else {
-		env.Machines = strings.Split(*etcdmachines, "|")
-	}
-}
 
 type Service struct {
 	Title   string           `json:"-"`
@@ -82,6 +27,7 @@ func (s *Service) Serve() {
 	s.shutdown()
 	s.register()
 	go s.heartbeat()
+	go env.refresh()
 	http.ListenAndServe(fmt.Sprintf(":%v", env.Port), s.Handler)
 }
 
@@ -118,20 +64,4 @@ func (s *Service) shutdown() {
 			os.Exit(0)
 		}
 	}()
-}
-
-func getEnvHost() (*string, error) {
-	host := os.Getenv("MONGOLAR_SERVICES_HOST")
-	if host == "" {
-		return &host, errors.New("MONGOLAR_SERVICES_HOST is not set, service host environement value is required.")
-	}
-	return &host, nil
-}
-
-func getEnvEtcdMachines() (*string, error) {
-	etcdmachines := os.Getenv("MONGOLAR_ETCD_MACHINES")
-	if etcdmachines == "" {
-		return &etcdmachines, errors.New("MONGOLAR_ETCD_MACHINES is not set, etcd machines environmental value is required.")
-	}
-	return &etcdmachines, nil
 }
