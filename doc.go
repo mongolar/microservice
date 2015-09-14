2e62c7e3da2dc88c94482cee37abcf0383913075
Jason Smith <jasonrichardsmith@gmail.com>
2015-09-14 12:23:32 -0400

    removed tabs in doc.go

--- doc.go
+++ doc.go
@@ -2,17 +2,17 @@
 // register a microservice with vulcand as a backend.
 
 /*
-	The service package is a wrapper tool to automaticaly declare and
-	register a microservice with vulcand as a backend.
+The service package is a wrapper tool to automaticaly declare and
+register a microservice with vulcand as a backend.
 
-	The wrapper takes standard http handlers and will fall back to the
-	DefaultServeMux if no Handler is provided
+The wrapper takes standard http handlers and will fall back to the
+DefaultServeMux if no Handler is provided
 
 
 
-	The package also allows you to declare a service as private requiring.
-	If a package is defined as private it will register a private key for
-	the service and update its randomly generated security key
+The package also allows you to declare a service as private requiring.
+If a package is defined as private it will register a private key for
+the service and update its randomly generated security key
 */
 
 package service
